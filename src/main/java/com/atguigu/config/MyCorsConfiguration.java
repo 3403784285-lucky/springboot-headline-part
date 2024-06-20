7d52b1f6d9f507440c5d537ea2fa47f75aace280
lzh <2212384795@qq.com>
2024-06-20 14:57:06 +0800

    解决跨域

---
+++ src/main/java/com/atguigu/config/MyCorsConfiguration.java
@@ -0,0 +1,24 @@
+package com.atguigu.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class MyCorsConfiguration {
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true); // 允许cookies跨域
+        //config.addAllowedOrigin("http://121.41.107.127:5173"); // 设置允许的源
+        config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedHeader("*"); // 允许任何头
+        config.addAllowedMethod("*"); // 允许任何方法（GET, POST, PUT, DELETE 等）
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config); // 对所有API生效
+        return new CorsFilter(source);
+    }
+}
