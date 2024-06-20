4ff450ac70be62d227e42160a3ab0ab7756c68db
lzh <2212384795@qq.com>
2024-06-21 00:22:19 +0800

    添加通过 spu 对 sku 分类接口的SpuMapper

---
+++ src/main/java/com/atguigu/mapper/SpuMapper.java
@@ -0,0 +1,18 @@
+package com.atguigu.mapper;
+
+import com.atguigu.pojo.Spu;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author LoveF
+* @description 针对表【spu】的数据库操作Mapper
+* @createDate 2024-06-20 19:45:44
+* @Entity com.atguigu.pojo.Spu
+*/
+public interface SpuMapper extends BaseMapper<Spu> {
+
+}
+
+
+
+
