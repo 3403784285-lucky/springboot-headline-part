6d604a63aa1a58502f45013c1d8d4411b31851f2
lzh <2212384795@qq.com>
2024-06-22 20:45:48 +0800

    修改房屋图片，上传到oss托管

--- src/main/java/com/atguigu/controller/FileController.java
+++ src/main/java/com/atguigu/controller/FileController.java
@@ -2,6 +2,7 @@ package com.atguigu.controller;
 
 import com.atguigu.pojo.Sku;
 import com.atguigu.service.SkuService;
+import com.atguigu.utils.AliOssUtil;
 import com.atguigu.utils.Result;
 import com.atguigu.utils.ResultCodeEnum;
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
@@ -16,6 +17,7 @@ import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.UUID;
 
 @RestController
 @CrossOrigin
@@ -25,9 +27,8 @@ public class FileController {
     @Autowired
     private SkuService skuService;
 
-    // 上传文件保存的路径
-    private static final String UPLOAD_DIR = "img/";
-
+    @Autowired
+    private AliOssUtil aliOssUtil;
 
     @PostMapping("save")
     public Result handleFileUpload(@RequestParam("file") MultipartFile file,
@@ -44,48 +45,39 @@ public class FileController {
                                    @RequestParam("houseCity") String houseCity,
                                    @RequestParam("houseOnto") String houseOnto,
                                    @RequestParam("unitType") String unitType) {
-        // 处理文件上传逻辑
         if (file.isEmpty()) {
             return Result.build(null, ResultCodeEnum.FILE_NULL);
         }
 
         try {
-            // 获取上传的文件名
-            String fileName = file.getOriginalFilename();
-            String fileP="E:\\test-vue-big\\public\\"+UPLOAD_DIR;
-            String filePCopy="http://127.0.0.1:5173/"+UPLOAD_DIR;
+            String originalFilename = file.getOriginalFilename();
+            String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+            String objectName = UUID.randomUUID().toString() + extension;
 
+            String fileUrl = aliOssUtil.upload(file.getBytes(), objectName);
 
-            // 构建上传路径
-            Path uploadPath =Paths.get(fileP);
-
-
-            // 如果目录不存在，则创建目录
-            if (!Files.exists(uploadPath)) {
-                Files.createDirectories(uploadPath);
-            }
-
-            // 构建文件存储路径
-            Path filePath = uploadPath.resolve(fileName);
-
-            if((new File(filePath.toString())).exists()){
-
-            }else{
-                Files.copy(file.getInputStream(), filePath);
-            }
-            System.out.println(filePath.toUri());
-            // 将文件保存到本地
-
-            Sku sku=new Sku(Integer.parseInt(skuId),Integer.parseInt(spuId),houseName,Double.parseDouble(houseSize),houseType,new BigDecimal(housePrice),housePosition,houseStatus,houseOnto,filePCopy+fileName,houseCity,unitType,Integer.parseInt(houseOwner));
+            Sku sku = new Sku(
+                    Integer.parseInt(skuId),
+                    Integer.parseInt(spuId),
+                    houseName,
+                    Double.parseDouble(houseSize),
+                    houseType,
+                    new BigDecimal(housePrice),
+                    housePosition,
+                    houseStatus,
+                    houseOnto,
+                    fileUrl,
+                    houseCity,
+                    unitType,
+                    Integer.parseInt(houseOwner)
+            );
             skuService.updateById(sku);
 
-            // 返回上传成功的消息
             return Result.ok(sku);
         } catch (IOException e) {
             e.printStackTrace();
+            return Result.build(null, 500, "error");
         }
-        return Result.build(null,500,"error");
-
     }
     @PostMapping("saveNoFile")
     public Result handleFileUpload(
