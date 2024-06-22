615418d2826952ed1da9ab55005fcdddd1d0b127
lzh <2212384795@qq.com>
2024-06-22 20:28:29 +0800

    添加根据获取指定的订单信息

---
+++ src/main/java/com/atguigu/vo/OrderStatus.java
@@ -0,0 +1,33 @@
+package com.atguigu.vo;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+public class OrderStatus implements Serializable {
+    // 房子头像
+    private String housePic;
+
+    //房子名称
+    private String houseName;
+
+    // 用户id
+    private Integer userId;
+
+    //用户昵称
+    private String nickname;
+
+    // 订单描述
+    private String description;
+
+    // 订单号
+    private Integer orderSkuId;
+
+    private String status;
+    //创建时间
+    @JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date orderCreateTime;
+}
